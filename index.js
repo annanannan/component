4093d3e622993288c5c95c01a1fc6d81c7ed7c0a
annanannan <1406571075@qq.com>
2020-03-24 10:17:26 +0800

    add

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 $(function() {
-  var myChart = echarts.init($('.main2').get(0));
+
+  var data = [
+    ['Java', '1', '降', '-0.01%'],
+    ['C', '2', '升', '+2.44%'],
+    ['Python', '3', '升','+1.41%'],
+    ['C++', '4', '降', '-2.58%'],
+    ['C#', '5', '升', '+2.07%'],
+    ['Visual Basic .NET', '6', '降', '-1.17%'],
+    ['JavaScript', '7', '降', '-0.85%']
+  ];
+
+  var container = $('.a1');
+
+  var hot = new Handsontable(container.get(0), {
+    data: data,
+    contextMenu: true,
+    manualRowResize : true,
+    manualColumnResize : true,
+    rowHeaders: false,
+    colWidths:150,
+    rowHeights:30,
+    colHeaders: [
+      '语言名称',
+      '排名',
+      '升或降',
+      '变化幅度'
+    ]
+  });
+                    
+  var myChart = echarts.init($('.a2').get(0));
 
   var option = {
     title: {
@@ -21,34 +50,5 @@ $(function() {
   };
 
   myChart.setOption(option);
-        
-  var data = [
-    ['Java', '1', '降', '-0.01%'],
-    ['C', '2', '升', '+2.44%'],
-    ['Python', '3', '升','+1.41%'],
-    ['C++', '4', '降', '-2.58%'],
-    ['C#', '5', '升', '+2.07%'],
-    ['Visual Basic .NET', '6', '降', '-1.17%'],
-    ['JavaScript', '7', '降', '-0.85%']
-  ];
-
-  var container = $('.main1');
-
-  var hot = new Handsontable(container.get(0), {
-    data: data,
-    contextMenu: true,
-    manualRowResize : true,
-    manualColumnResize : true,
-    rowHeaders: false,
-    colWidths:150,
-    rowHeights:30,
-    colHeaders: [
-      '语言名称',
-      '排名',
-      '升或降',
-      '变化幅度'
-    ]
-  });
-            
-  return hot;
+  return hot;   
 });
